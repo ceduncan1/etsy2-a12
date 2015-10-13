180bba81467bca0bd967286e3f3f86ac545a4375
Cary Duncan <duncan.cary@gmail.com>
2015-10-13 08:59:40 -0400

    Completed new JS, added CSS

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,22 @@
 
 (function () {
 
-  console.log('It Works!');
+  var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=dhz2o347b10mvvul6vdr8g9k&keywords=space+cat&includes=Images,Shop';
+
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  }).then(function (response) {
+    var templateString = $('#itemTemplate').text();
+    var templateFunction = _.template(templateString);
+
+    _.each(response.results, function (item, index) {
+      console.log(item);
+      if (index <= 23) {
+        var itemHTML = templateFunction(item);
+        $('.items').append(itemHTML);
+      }
+    });
+  });
 })();
